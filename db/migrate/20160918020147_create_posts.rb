a18d729fdfb04732fe70626a112c7c54f62a3ed6
venkat <lvenkatreddy13@gmail.com>
2016-09-20 10:50:33 +0530

    first commit

---
+++ db/migrate/20160918020147_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :description
+      t.string :text
+
+      t.timestamps null: false
+    end
+  end
+end
